57306037740d75442657e714597ee321f78ff0cc
HarleyKwyn <kwyn.meagher@gmail.com>
2014-12-24 23:20:17 -0500

    Fix socket issues

--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+  var socket;
+
   $('.js-connect').on('click', function() {
+    $('.connected').hide();
     socket = new WebSocket("ws://10.0.0.11:8080/");
+    console.log('created new socket')
     socket.onopen = function(evt) {
+      console.log('opened socket')
       $('.connected').show();
       $('.disconnected').hide();
     };
