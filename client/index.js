5e333a6a115e65cc7f89e16e342ea3b41bfbf0e1
HarleyKwyn <kwyn.meagher@gmail.com>
2014-12-24 23:02:02 -0500

    Working color picker'

--- client/index.js
+++ client/index.js
@@ -1,21 +1,33 @@
-var socket = new WebSocket("ws://10.0.0.11:8080/");
-socket.onopen = function(evt) {
-  $('.connected').show();
-  $('.disconnected').hide();
-};
-socket.onclose = function(evt) {
-  $('.connected').hide();
-  $('.disconnected').show();
-}
-socket.onerr = function(evt) {
-  $('.error').show();
-}
+$(document).ready(function() {
+  $('.js-connect').on('click', function() {
+    socket = new WebSocket("ws://10.0.0.11:8080/");
+    socket.onopen = function(evt) {
+      $('.connected').show();
+      $('.disconnected').hide();
+    };
+    socket.onclose = function(evt) {
+      $('.connected').hide();
+      $('.disconnected').show();
+    }
+    socket.onerr = function(evt) {
+      $('.error').show();
+    }
+  });
 
-$(".color").spectrum({
-  flat: true,
-  color: "#f00",
-  change: function(color) {
-    console.log(color);
-    socket.send(color.toHexString());
-  }
+  $(".color").pickAColor({
+    showSpectrum: true,
+    showSavedColors: true,
+    saveColorsPerElement: true,
+    fadeMenuToggle: true,
+    showAdvanced: true,
+    showBasicColors: true,
+    showHexInput: true,
+    allowBlank: true,
+    inlineDropdown: true
+  });
+
+  $(".color").on("change", function(event) {
+    console.log(event.target.value);
+    socket.send(event.target.value);
+  });
 });
